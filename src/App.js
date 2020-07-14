75f59d796f3780a775b2879b99d5a95487d0287c
AnnaFragoso <anna.barbara.fragoso@hotmail.com>
2020-07-13 22:51:54 -0300

    reactotron

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import Routes from './routes';
 import { Router } from 'react-router-dom';
 import history from './services/history';
+import './config/ReactotronConfig';
 
 function App() {
   return (
     <Router history={history}>
-      <Routes />;
+      <Routes />
     </Router>
   )
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 /* eslint-disable react/react-in-jsx-scope */
 /* eslint-disable react/jsx-no-comment-textnodes */
+import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import SignUp from '../pages/SignUp';
 import SignIn from '../pages/SignIn';
